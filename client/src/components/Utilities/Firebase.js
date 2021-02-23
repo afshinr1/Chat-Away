ae95e77d6e8aae26ec9fe0a4ef652d6616d97f71
Afshin Rahman <60902229+afshinr1@users.noreply.github.com>
2021-02-23 15:27:21 -0700

    Update Firebase.js

--- client/src/components/Utilities/Firebase.js
+++ client/src/components/Utilities/Firebase.js
@@ -2,18 +2,6 @@
 
 import firebase from "firebase";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyChpsf5IB9WVv1GnyOZTn0Px3QNCUg41w0",
-  authDomain: "seng-51.firebaseapp.com",
-  databaseURL: "https://seng-51.firebaseio.com",
-  projectId: "seng-51",
-  storageBucket: "seng-51.appspot.com",
-  messagingSenderId: "1026081474984",
-  appId: "1:1026081474984:web:5471c6141c6a28058d359d",
-  measurementId: "G-1B3B3YYV45",
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
 
 export default firebase;
